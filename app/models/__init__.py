793ea2742eac942880e5c5464660e9f3313175b1
Hakjun Kim <hakie2kim@gmail.com>
2025-02-22 15:18:16 +0900

    association 연관 테이블 설정

---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .link import Link
+from .association import link_shares
--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,13 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
 from app.db.database import Base
+from app.models.association import link_shares
+
 
 class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
     hashed_password = Column(String)
 
-    links = relationship("Link", back_populates="owner")
-    shared_links = relationship("LinkShare", back_populates="shared_user")
+    shared_links = relationship('Link', secondary=link_shares, back_populates='shared_users')
